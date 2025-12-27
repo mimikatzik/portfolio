fc8e55e905f94fcb460db8226de111769308e073
mimexx <progristillidan@gmail.com>
2025-12-28 02:55:45 +0300

    finally-tochno

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173
-  }  
+  // server: {
+  //   host: '0.0.0.0',
+  //   port: 5173
+  // }  
 })
 
